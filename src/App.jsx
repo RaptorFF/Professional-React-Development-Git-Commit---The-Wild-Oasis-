a9402e3a102df877345105e4c7255f9b404558b7
RaptorFF <f.filipovic84@gmail.com>
2026-01-04 12:16:13 +0100

    Uploading images to Supabase-solved issue

---
+++ src/App.jsx
@@ -0,0 +1,69 @@
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import GlobalStyles from "./styles/GlobalStyles";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { Toaster } from "react-hot-toast";
+
+import Dashboard from "./pages/Dashboard.jsx";
+import Account from "./pages/Account.jsx";
+import Bookings from "./pages/Bookings.jsx";
+import Cabins from "./pages/Cabins.jsx";
+import Login from "./pages/Login.jsx";
+import PageNotFound from "./pages/PageNotFound.jsx";
+import Settings from "./pages/Settings.jsx";
+import Users from "./pages/Users.jsx";
+import AppLayout from "./ui/AppLayout.jsx";
+
+// Create a QueryClient instance for React Query with default options
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 0,
+    },
+  },
+});
+
+function App() {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} />
+      <GlobalStyles />
+      <BrowserRouter>
+        <Routes>
+          <Route element={<AppLayout />}>
+            {/* Redirect root to /dashboard */}
+            <Route index element={<Navigate replace to="dashboard" />} />
+            <Route path="dashboard" element={<Dashboard />} />
+            <Route path="account" element={<Account />} />
+            <Route path="bookings" element={<Bookings />} />
+            <Route path="cabins" element={<Cabins />} />
+            <Route path="settings" element={<Settings />} />
+            <Route path="users" element={<Users />} />
+          </Route>
+          <Route path="login" element={<Login />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </BrowserRouter>
+
+      {/* Toaster component for displaying toast notifications */}
+      <Toaster
+        position="top-center"
+        gutter={12}
+        containerStyle={{ margin: "8px" }}
+        toastOptions={{
+          success: { duration: 3000 },
+          error: { duration: 5000 },
+          style: {
+            fontSize: "16px",
+            maxWidth: "500px",
+            padding: "16px 24px",
+            backgroundColor: "var(--color-grey-0)",
+            color: "var(--color-grey-700)",
+          },
+        }}
+      />
+    </QueryClientProvider>
+  );
+}
+
+export default App;
---
+++ src/features/cabins/CreateCabinForm.jsx
@@ -0,0 +1,147 @@
+import { useForm } from "react-hook-form";
+
+import Input from "../../ui/Input";
+import Form from "../../ui/Form";
+import Button from "../../ui/Button";
+import FileInput from "../../ui/FileInput";
+import Textarea from "../../ui/Textarea";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { createCabin } from "../../services/apiCabins";
+import toast from "react-hot-toast";
+import FormRow from "../../ui/FormRow";
+
+function CreateCabinForm() {
+  // Initialize the form using react-hook-form
+  const {
+    register, // Register form fields
+    handleSubmit, // Handle form submission
+    reset, // Reset the form
+    getValues, // Get current form values
+    formState, // Form validation errors
+  } = useForm();
+
+  // Extract errors from form state
+  const { errors } = formState;
+
+  // Get the query client for invalidating queries
+  const queryClient = useQueryClient();
+
+  const { mutate, isPending: isCreating } = useMutation({
+    mutationFn: createCabin,
+    onSuccess: (data) => {
+      toast.success("Cabin created:", data);
+      // Invalidate and refetch cabins query to reflect the new cabin
+      queryClient.invalidateQueries({ queryKey: ["cabins"] });
+      reset(); // Reset the form after successful submission
+    },
+    onError: (error) => {
+      toast.error(error.message);
+    },
+  });
+
+  // Handle form submission
+  function onSubmit(data) {
+    //console.log(data);
+    mutate({ ...data, image: data.image[0] });
+  }
+
+  // Handle form submission errors
+  function onError(errors) {
+    //console.log(errors);
+  }
+
+  return (
+    <Form onSubmit={handleSubmit(onSubmit, onError)}>
+      <FormRow label="Cabin name" errors={errors.name && errors.name.message}>
+        <Input
+          type="text"
+          id="name"
+          {...register("name", { required: "This field is required" })}
+          disabled={isCreating}
+        />
+      </FormRow>
+
+      <FormRow
+        label="Maximum capacity"
+        errors={errors.maxCapacity && errors.maxCapacity.message}
+      >
+        <Input
+          type="number"
+          id="maxCapacity"
+          {...register("maxCapacity", {
+            required: "This field is required",
+            min: { value: 1, message: "Capacity must be at least 1" },
+          })}
+          disabled={isCreating}
+        />
+      </FormRow>
+
+      <FormRow
+        label="Regular price"
+        errors={errors.regularPrice && errors.regularPrice.message}
+      >
+        <Input
+          type="number"
+          id="regularPrice"
+          {...register("regularPrice", {
+            required: "This field is required",
+          })}
+          disabled={isCreating}
+        />
+      </FormRow>
+
+      <FormRow
+        label="Discount"
+        errors={errors.discount && errors.discount.message}
+      >
+        <Input
+          type="number"
+          id="discount"
+          {...register("discount", {
+            required: "This field is required",
+            // Custom validation to ensure discount is less than regular price
+            validate: (value) =>
+              value <= getValues().regularPrice ||
+              "Discount cannot be greater than regular price",
+          })}
+          disabled={isCreating}
+          defaultValue={0}
+        />
+      </FormRow>
+
+      <FormRow
+        label="Description for website"
+        errors={errors.description && errors.description.message}
+      >
+        <Textarea
+          type="number"
+          id="description"
+          {...register("description")}
+          defaultValue=""
+          disabled={isCreating}
+        />
+      </FormRow>
+
+      <FormRow
+        label="Photo"
+        disabled={isCreating}
+        errors={errors.location && errors.location.message}
+      >
+        <FileInput id="image" accept="image/*" {...register("image")} />
+      </FormRow>
+
+      <FormRow>
+        {/* type is an HTML attribute! */}
+        <Button
+          variation="secondary"
+          type="reset" /* Reset button to clear the form */
+        >
+          Cancel
+        </Button>
+        <Button disabled={isCreating}>Add cabin</Button>
+      </FormRow>
+    </Form>
+  );
+}
+
+export default CreateCabinForm;
