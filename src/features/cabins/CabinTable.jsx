4c37ba300526f37d38b3220d041a58b29176f563
RaptorFF <f.filipovic84@gmail.com>
2026-01-16 14:01:31 +0100

    Filtering Cabins

--- src/features/cabins/CabinTable.jsx
+++ src/features/cabins/CabinTable.jsx
@@ -3,12 +3,28 @@ import CabinRow from "./CabinRow";
 import { useCabins } from "./useCabins";
 import Table from "../../ui/Table";
 import Menus from "../../ui/Menus";
+import { useSearchParams } from "react-router-dom";
 
 function CabinTable() {
   // Use custom hook to fetch cabins data
   const { isLoading, cabins } = useCabins();
+  const [searchParams] = useSearchParams();
+
+  // Show spinner while loading
 
   if (isLoading) return <Spinner />;
+
+  const discountFilter = searchParams.get("discount") || "all"; // Get discount filter from URL params
+
+  // Filter cabins based on discount filter
+  const filteredCabins = cabins.filter((cabin) => {
+    if (discountFilter === "with-discount") {
+      return cabin.discount > 0;
+    } else if (discountFilter === "no-discount") {
+      return cabin.discount === 0;
+    }
+    return true; // "all" or no filter
+  });
 
   return (
     <Menus>
@@ -22,7 +38,7 @@ function CabinTable() {
         </Table.Header>
 
         <Table.Body
-          data={cabins} //data prop to pass cabins array
+          data={filteredCabins} //data prop to pass cabins array
           render={(cabin) => <CabinRow cabin={cabin} key={cabin.id} />} //render prop to render each cabin row
         />
       </Table>
