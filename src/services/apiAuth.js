ac13464b465088f799d04c53cfd8bf7e58c7810f
RaptorFF <f.filipovic84@gmail.com>
2026-02-03 13:25:57 +0100

    Signup User

--- src/services/apiAuth.js
+++ src/services/apiAuth.js
@@ -1,5 +1,23 @@
 import supabase from "./supabase";
 
+export async function signup({ fullName, email, password }) {
+  const { data, error } = await supabase.auth.signUp({
+    email,
+    password,
+    // options to set additional user metadata during signup
+    options: {
+      data: {
+        full_name: fullName,
+        avatar: "",
+      },
+    },
+  });
+  if (error) {
+    throw new Error(error.message);
+  }
+  return data;
+}
+
 export async function login({ email, password }) {
   let { data, error } = await supabase.auth.signInWithPassword({
     email,
